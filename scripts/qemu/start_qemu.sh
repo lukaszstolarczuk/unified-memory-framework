f5d0d08c127724fc1862260f4db671138b098a0e
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2024-08-08 20:40:41 +0200

    [CI] don't extra clone repo in QEMU job
    just use the one available in the workflow - copy it into image.

--- scripts/qemu/start_qemu.sh
+++ scripts/qemu/start_qemu.sh
@@ -8,7 +8,7 @@ set -e
 config_file=$1
 
 # Parse the config file to get topology info and fix escaped single quotes
-parsed_config=$(python3 scripts/qemu/parse_config.py ${config_file} | sed s/''\''/'/g)
+parsed_config=$(python3 "$(dirname $0)/parse_config.py" ${config_file} | sed s/''\''/'/g)
 
 sudo qemu-system-x86_64 \
     -drive file=./ubuntu-23.04-server-cloudimg-amd64.img,format=qcow2,index=0,media=disk,id=hd \
